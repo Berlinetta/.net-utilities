a9b97f807ed8f3621e01f4a53d96b87634cab2c0
Wang <WangTo@moodys.com>
2018-03-14 18:33:01 +0800

    Add CsvUtilityTest

---
+++ Utilities/CsvUtilityTest/CsvWriterTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CsvUtility;
+
+namespace CsvUtilityTest
+{
+    [TestClass]
+    public class CsvWriterTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string filePath = string.Format(@"{0}\test.csv", AppDomain.CurrentDomain.BaseDirectory);
+            var writer = new CsvWriter(filePath);
+            string[] values = new string[] { "11", "22,2", "3,3" };
+            writer.WriteRecord(values);
+            writer.Close();
+        }
+    }
+}
